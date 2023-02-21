79fc2b6f2a6c4b152d1a0aaacc46af762d34f4cd
IbubussI <Oleksii_Cherednichenko@epam.com>
2023-02-21 15:57:51 +0200

    Move classes to packages, create stub methods in controller

---
+++ src/main/java/com/ws/unit/stats/application/UnitStatsApplication.java
@@ -0,0 +1,13 @@
+package com.ws.unit.stats.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitStatsApplication.class, args);
+	}
+
+}
