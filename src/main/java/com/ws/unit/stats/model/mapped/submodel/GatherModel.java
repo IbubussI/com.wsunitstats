d3b68d2e5bee643df469aa88915cfc15e3438633
ibubussi <->
2023-02-26 12:22:43 +0200

    Add (implement) Movement and Gather models

--- src/main/java/com/ws/unit/stats/model/mapped/submodel/GatherModel.java
+++ src/main/java/com/ws/unit/stats/model/mapped/submodel/GatherModel.java
@@ -1,7 +1,80 @@
 package com.ws.unit.stats.model.mapped.submodel;
 
 public class GatherModel {
+    private Double bagSize;
+    private Double findTargetDistance;
+    private Double gatherDistance;
+    private Double putDistance;
+    private Double perSecond;
+    private String envType;
+    private String resource;
 
-    //TODO
+    public Double getBagSize() {
+        return bagSize;
+    }
 
+    public void setBagSize(Double bagSize) {
+        this.bagSize = bagSize;
+    }
+
+    public Double getFindTargetDistance() {
+        return findTargetDistance;
+    }
+
+    public void setFindTargetDistance(Double findTargetDistance) {
+        this.findTargetDistance = findTargetDistance;
+    }
+
+    public Double getGatherDistance() {
+        return gatherDistance;
+    }
+
+    public void setGatherDistance(Double gatherDistance) {
+        this.gatherDistance = gatherDistance;
+    }
+
+    public Double getPutDistance() {
+        return putDistance;
+    }
+
+    public void setPutDistance(Double putDistance) {
+        this.putDistance = putDistance;
+    }
+
+    public Double getPerSecond() {
+        return perSecond;
+    }
+
+    public void setPerSecond(Double perSecond) {
+        this.perSecond = perSecond;
+    }
+
+    public String getEnvType() {
+        return envType;
+    }
+
+    public void setEnvType(String envType) {
+        this.envType = envType;
+    }
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public String toString() {
+        return "GatherModel{" +
+                "bagSize=" + bagSize +
+                ", findTargetDistance=" + findTargetDistance +
+                ", gatherDistance=" + gatherDistance +
+                ", putDistance=" + putDistance +
+                ", perSecond=" + perSecond +
+                ", envType='" + envType + '\'' +
+                ", resource='" + resource + '\'' +
+                '}';
+    }
 }
