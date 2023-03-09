9ae8892eb0d7f127a04fa81674ba4457c59b455c
IbubussI <Oleksii_Cherednichenko@epam.com>
2023-03-09 23:27:58 +0200

    Add domain model for weapon

---
+++ com.wsunitstats.domain/src/main/java/com/wsunitstats/domain/submodel/weapon/DistanceModel.java
@@ -0,0 +1,14 @@
+package com.wsunitstats.domain.submodel.weapon;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DistanceModel {
+    private Double max;
+    private Double min;
+    private Double stop;
+}
