753850b3ad2944d4f5406d77160ea6f46fa0e7c9
ibubussi <->
2023-04-08 03:11:25 +0300

    Correct typo in footer

--- com.wsunitstats.ui/src/components/Footer/index.jsx
+++ com.wsunitstats.ui/src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer>
       <h6>About</h6>
-      <p class="text-justify">This is a free and open-source project desined to to view War Selection in-game unit stats. Feel free to contact, ask any questions, report issues, make suggestions and contribute</p>
+      <p class="text-justify">This is a free and open-source project desined to view War Selection in-game unit stats. Feel free to contact, ask any questions, report issues, make suggestions and contribute</p>
       <hr />
       <p class="underline-text">
         Created by: IbubussI, 2023
