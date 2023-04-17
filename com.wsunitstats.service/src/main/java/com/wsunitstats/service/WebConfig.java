71749602cbd6c218b2a2a2d6e7cfbcc533175cfc
IbubussI <->
2023-04-18 01:38:06 +0300

    Add config to redirect all static queries to React root page (for SPA pattern)

--- com.wsunitstats.service/src/main/java/com/wsunitstats/service/WebConfig.java
+++ com.wsunitstats.service/src/main/java/com/wsunitstats/service/WebConfig.java
@@ -2,6 +2,7 @@ package com.wsunitstats.service;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,4 +11,17 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("forward:/index.html");
+        // Single directory level - no need to exclude "api"
+        registry.addViewController("/{x:[\\w\\-]+}")
+                .setViewName("forward:/index.html");
+        // Multi-level directory path, need to exclude "api" on the first part of the path
+        registry.addViewController("/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}")
+                .setViewName("forward:/index.html");
+    }
+
 }
